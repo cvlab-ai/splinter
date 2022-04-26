e1ffab4e73f14d669e4cc8264e3f6c06dc49af8d
Jakub Lecki <lec.jakub@gmail.com>
2022-04-26 19:22:04 +0200

    exam-generator: Randomize internlines and margins, generate UUID, compress results to zip
    Signed-off-by: Jakub Lecki <lec.jakub@gmail.com>

--- generator/exam-generator/__main__.py
+++ generator/exam-generator/__main__.py
@@ -1,13 +1,31 @@
+import os
+import pathlib
+import typing as tp
+import zipfile
+
 from . import ExamGenerator, GeneratorConfig
 from .instruction_generator import generate_exam_instruction
+
+
+def zip_files(zip_file: str, filenames: tp.List[str]):
+    with zipfile.ZipFile(zip_file, 'w') as zipF:
+        for file in filenames:
+            zipF.write(file, compress_type=zipfile.ZIP_DEFLATED)
+            os.remove(file)
+
 
 
 def main():
     config = GeneratorConfig.random()
     exam_generator = ExamGenerator(config)
-    exam_generator.generate()
-    generate_exam_instruction(config.number_of_answers, "exam.txt")
 
+    base_filename = str(config.exam_id)
+    exam_generator.generate(base_filename)
+    config.export(f"exams/{base_filename}.json")
+    generate_exam_instruction(config.number_of_answers, f"{base_filename}.txt")
+
+    pathlib.Path(f"exams").mkdir(parents=True, exist_ok=True)
+    zip_files(f"exams/{base_filename}.zip", [f"{base_filename}.pdf", f"{base_filename}.txt"])
 
 if __name__ == "__main__":
     main()
