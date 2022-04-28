68c9f120fb441a745a1a93d05e7b788b386478a5
Jakub Lecki <lec.jakub@gmail.com>
2022-04-28 17:43:58 +0200

    exam-generator: Add base argument handling.
    now it is possible to specify:
    * destination directory
    * number of generated exams
    * if files shoud be compressed
    * test mode - always generate exam with the same name - TESTING OPTION
    
    Signed-off-by: Jakub Lecki <lec.jakub@gmail.com>

--- generator/exam-generator/__main__.py
+++ generator/exam-generator/__main__.py
@@ -5,27 +5,44 @@ import zipfile
 
 from . import ExamGenerator, GeneratorConfig
 from .instruction_generator import generate_exam_instruction
+from .generator_args import GeneratorArgs
 
 
 def zip_files(zip_file: str, filenames: tp.List[str]):
-    with zipfile.ZipFile(zip_file, 'w') as zipF:
+    with zipfile.ZipFile(zip_file, "w") as zipF:
         for file in filenames:
             zipF.write(file, compress_type=zipfile.ZIP_DEFLATED)
             os.remove(file)
 
 
+def generate_package(name: str, config: GeneratorConfig):
+    exam_generator = ExamGenerator(config)
+    exam_generator.generate(name)
+    config.export(f"{name}.json")
+    generate_exam_instruction(config.number_of_answers, f"{name}.txt")
+
 
 def main():
-    config = GeneratorConfig.random()
-    exam_generator = ExamGenerator(config)
+    args = GeneratorArgs().parse_args()
+    # Make sure output directory exists
+    pathlib.Path(args.destdir).mkdir(parents=True, exist_ok=True)
 
-    base_filename = str(config.exam_id)
-    exam_generator.generate(base_filename)
-    config.export(f"exams/{base_filename}.json")
-    generate_exam_instruction(config.number_of_answers, f"{base_filename}.txt")
+    for _ in range(args.number):
+        config = GeneratorConfig.random()
+        base_filename = f"{args.destdir}/{str(config.exam_id)}"
+        if args.test:
+            base_filename = f"{args.destdir}/exam"
 
-    pathlib.Path(f"exams").mkdir(parents=True, exist_ok=True)
-    zip_files(f"exams/{base_filename}.zip", [f"{base_filename}.pdf", f"{base_filename}.txt"])
+        generate_package(base_filename, config)
+        if args.zip:
+            zip_files(
+                f"{base_filename}.zip",
+                [
+                    f"{base_filename}.pdf",
+                    f"{base_filename}.txt",
+                ],
+            )
+
 
 if __name__ == "__main__":
     main()
