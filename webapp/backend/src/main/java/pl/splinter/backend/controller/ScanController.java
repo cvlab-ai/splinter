d31983035ff857e70e908fc1310a07716c5420dd
Mateusz <mateuszkepes@outlook.com>
2022-06-14 15:27:08 +0200

    Add basic backend.

---
+++ webapp/backend/src/main/java/pl/splinter/backend/controller/ScanController.java
@@ -0,0 +1,23 @@
+package pl.splinter.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.splinter.backend.service.ScanData;
+import pl.splinter.backend.service.ScanService;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/scan")
+public class ScanController {
+    private final ScanService scanService;
+
+    @Autowired
+    public ScanController(ScanService service) {
+        this.scanService = service;
+    }
+
+    @PostMapping("/save")
+    public void saveScan(@RequestBody ScanData data) {
+        scanService.saveScan(data);
+    }
+}
