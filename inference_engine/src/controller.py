b0db62d65f494d0051ee393c4553709be1a54b65
Marek Hering <marek.hering99@gmail.com>
2022-10-03 21:39:02 +0200

    add package setup

--- inference_engine/src/controller.py
+++ inference_engine/src/controller.py
@@ -1,15 +1,22 @@
+from .exam_storage import ExamStorage
 from .dto import CheckExamsDTO, CheckExamDTO, GenerateExamKeyDTO
 
 
 class Controller:
     @staticmethod
     def check_exam(request: CheckExamDTO):
-        raise NotImplementedError
+        return Controller._check_exam(request.exam_path, request.exam_name)
 
     @staticmethod
     def check_exams(request: CheckExamsDTO):
-        raise NotImplementedError
+        for exam_name in ExamStorage.get_exams_names(request.exam_path):
+            Controller._check_exam(request.exam_path, exam_name)
 
     @staticmethod
     def generate_exam_key(request: GenerateExamKeyDTO):
         raise NotImplementedError
+
+    @staticmethod
+    def _check_exam(exam_path: str, exam_name: str):
+        exam_image = ExamStorage.get_exam_image(exam_path, exam_name)
+        raise NotImplementedError  # TODO Run inference
--- inference_engine/src/inferenece_runner/inference_model.py
+++ inference_engine/src/inferenece_runner/inference_model.py
@@ -1,7 +1,9 @@
 from openvino.runtime import Core
+from exam_solver import ExamSolver
 import tensorflow as tf
 import numpy as np
 import keras.backend as K
+import typing as tp
 
 
 def macro_accuracy(y_true, y_pred):
@@ -9,10 +11,14 @@ def macro_accuracy(y_true, y_pred):
 
 
 class InferenceModel:
-    def __init__(self, model_path: str = 'model\\saved_model.xml'):
+    def __init__(self, model_path: str = 'model\\saved_model.xml', preprocessing: tp.Callable = None,
+                 postprocessing: tp.Callable = None):
         ie = Core()
         self.model = ie.compile_model(model=ie.read_model(model=model_path), device_name="CPU")
         self.output_layer = self.model.output(0)
+        self.preprocessing = preprocessing or self._preprocessing
+        self.postprocessing = postprocessing or self._postprocessing
+        self.exam_solver = ExamSolver(None) # TODO remove init param
 
     @staticmethod
     def convert_model_from_h5_to_pb(input_path, output_path):
@@ -23,6 +29,18 @@ class InferenceModel:
         tf.saved_model.save(model, output_path)
         return True
 
-    def run_inference(self, input_value: np.array):
-        input_value = np.expand_dims(input_value, 0)
-        return self.model([input_value])[self.output_layer]
+    def _preprocessing(self, inference_input: np.array) -> np.array:
+        inference_input[inference_input < 195] = 0
+        _, answer_card, box_positions = self.exam_solver.pipeline(inference_input)
+        print(box_positions.shape)
+        if len(inference_input.shape) < 4:
+            inference_input = np.expand_dims(inference_input, 0)
+        return inference_input
+
+    def _postprocessing(self, model_output: np.array) -> tp.Union[tp.List[int], tp.List[tp.List[int]]]:
+        return model_output
+
+    def run_inference(self, input_image: np.array):
+        model_input = self.preprocessing(input_image)
+        model_output = self.model([model_input])[self.output_layer]
+        return self.postprocessing(model_output)
