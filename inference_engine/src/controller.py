ba297782287bdf99771ebaec53155185ab22ecc2
Marek Hering <marek.hering99@gmail.com>
2022-10-07 18:28:23 +0200

    add models nad logging

--- inference_engine/src/controller.py
+++ inference_engine/src/controller.py
@@ -1,4 +1,7 @@
+import logging
+
 import numpy as np
+from json import dumps
 
 from src.config import Config
 from .storage import Storage
@@ -25,8 +28,10 @@ class Controller:
     def _mark_detection(file_path: str, file_name: str):
         image = Storage.get_exam_image(file_path, file_name)
         answer_input, index_input = Preprocessing().process(image)
-        answer_result = AnswerModel(Config.paths.answer_model_path).inference(answer_input)
         index_result = IndexModel(Config.paths.index_model_path).inference(index_input)
+        logging.info(f"Detected index: {index_result}")
+        answer_result = AnswerModel(Config.paths.answer_model_path).inference(answer_input)
+        logging.info(f"Detected answers: {Controller._get_readable_answers(answer_result)}")
         json_result = Controller._create_output_json(answer_result, index_result)
         output_file = file_name.split('.')[0]
         Storage.set_exam_answer_json(file_path, output_file, json_result)
@@ -37,3 +42,9 @@ class Controller:
             "index": index,
             "answers": {i + 1: [int(answer) for answer in row] for i, row in enumerate(answers)}
         }
+
+    @staticmethod
+    def _get_readable_answers(answers: np.ndarray) -> str:
+        return dumps({i + 1: ', '.join(
+            [chr(ord('A') + idx) for idx, val in enumerate(answer) if val]
+        ) for i, answer in enumerate(answers) if np.any((answer == 1))}, indent=4)
