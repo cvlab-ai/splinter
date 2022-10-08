f2974f487d9985361ff5b6d7268ca7df2eaa9017
Marek Hering <marek.hering99@gmail.com>
2022-10-08 09:11:55 +0200

    add default extension for images in storage

--- inference_engine/main.py
+++ inference_engine/main.py
@@ -1,10 +1,11 @@
 import logging
 
 from src.controller import Controller
-from src.dto import CheckExamDTO
+from src.dto import CheckExamDTO, CheckExamsDTO
 
 logging.basicConfig(level=logging.INFO)
 
 
 if __name__ == '__main__':
-    Controller.check_exam(CheckExamDTO.parse_obj({'exam_path': 'exams', 'exam_name': 'image--001.jpg'}))
+    Controller.check_exams(CheckExamsDTO.parse_obj({'exam_path': 'exams'}))
+    # Controller.check_exam(CheckExamDTO.parse_obj({'exam_path': 'exams', 'exam_name': 'image--001.jpg'}))
--- inference_engine/src/config/exam_storage_config.py
+++ inference_engine/src/config/exam_storage_config.py
@@ -5,6 +5,7 @@ class ExamStorageConfig(BaseModel):
     url: str = "http://localhost"
     port: int = 8888
     answer_key_filename: str = "answer_key"
+    img_extension: str = "jpg"
 
     @property
     def full_url(self):
