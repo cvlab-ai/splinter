ec76f9b05b20862b2a9fe618b476717a83b19c62
Jakub Lecki <lec.jakub@gmail.com>
2022-10-25 23:39:38 +0200

    exam_storage: Implement creting exam zip archives.
    exam zip archives can be downloaded from
    
    http://splinter_exam_storage/zip<exam_id>
    
    example using curl:
    ```
    curl http://localhost:8888/zip/1 -u splinter --output 1.zip
    ```

---
+++ exam_storage/scripts/create_zip.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import sys
+from pathlib import Path
+import zlib
+
+
+STORAGE_BASEDIR = Path("/www/data/")
+ZIP_BASEDIR = Path(STORAGE_BASEDIR / "zip/")
+SPLINTER_BASEDIR = Path(STORAGE_BASEDIR / "splinter/")
+exam_path = Path(SPLINTER_BASEDIR / Path(sys.argv[1]).name)
+
+
+def crc(fileName):
+    prev = 0
+    for eachLine in open(fileName, "rb"):
+        prev = zlib.crc32(eachLine, prev)
+    return "%X" % (prev & 0xFFFFFFFF)
+
+
+def list_files(dir: Path):
+    files = []
+    for file in dir.iterdir():
+        if file.is_dir():
+            files.extend(list_files(file))
+        else:
+            file_crc = crc(file)
+            size = file.stat().st_size
+            input_filename = file.relative_to(STORAGE_BASEDIR)
+            output_filename = file.relative_to(exam_path)
+            files.append(f"{file_crc} {size} /{input_filename} /{output_filename}")
+    return files
+
+
+print(f"Compressing exam: {exam_path}")
+if not exam_path.is_dir():
+    sys.exit()
+
+ZIP_BASEDIR.mkdir(exist_ok=True)
+
+with open(ZIP_BASEDIR / exam_path.name, "w") as zip:
+    zip.writelines("\n".join(list_files(exam_path)) + "\n")
