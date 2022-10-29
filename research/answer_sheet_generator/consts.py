5413494bbf6f7ba7b6e62a75161eb097a00890cd
Jakub Debski <debskij99@gmail.com>
2022-10-29 14:16:29 +0200

    Research: Create better exam template (#41)
    * Prepare better exam template
    
    * Change directory of files
    
    * Remove top right square
    
    * Apply comments

---
+++ research/answer_sheet_generator/consts.py
@@ -0,0 +1,11 @@
+page_top = lambda x, y: f'<svg xmlns="http://www.w3.org/2000/svg" width="{x}" height="{y}" fill="white" stroke="#000" stroke-width="2" version="1.1">'
+
+page_bottom = "</svg>"
+
+rectangle = lambda x_pos, y_pos, w, h, fill_color, border_color, border_width: f'<rect x="{x_pos}" y="{y_pos}" width="{w}" height="{h}" style="fill:{fill_color};stroke:{border_color};stroke-width:{border_width}" />'
+
+text = lambda x_pos, y_pos, font_size, color, content: f'<text x="{x_pos}" y="{y_pos}" font-size="{font_size}" font-family="sans-serif" style="fill: {color}; stroke: {color}"> {content} </text>'
+
+# Those values are taken from previous template
+PAGE_HEIGHT = 3367
+PAGE_WIDTH = 2380 
