cabc4a2b7bd155a9e05d9d69a778155dfe798a02
Marek Hering <marek.hering99@gmail.com>
2022-11-08 11:32:28 +0100

    fix box model path

--- inference_engine/src/config/paths_config.py
+++ inference_engine/src/config/paths_config.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel
 
 
 class PathsConfig(BaseModel):
-    box_model_path: str = "data/model/saved_model.xml",
+    box_model_path: str = "data/model/saved_model.xml"
     ocr_model_path: str = "data/model/handwritten-english-recognition-0001.xml"
