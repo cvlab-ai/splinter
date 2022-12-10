e355df132ffaacb931f50fe688d870ca810a7c40
Marek Hering <marek.hering99@gmail.com>
2022-12-10 18:16:45 +0100

    handle lack of student index

--- inference_engine/src/model/box_model.py
+++ inference_engine/src/model/box_model.py
@@ -8,7 +8,8 @@ from src.utils import show_image
 class BoxModel(Model):
     def inference(self, _input: np.array, argmax: bool = False, *args, **kwargs):
         if argmax:
-            return [np.argmax(self.model([vals])[self.output_layer]) for vals in _input]
+            pred = [self.model([vals])[self.output_layer] for vals in _input]
+            return np.argmax(pred, axis=1).flatten(), np.max(pred, axis=1).flatten()
         else:
             return [(self.model([vals])[self.output_layer] > Config.inference.answer_threshold).astype(int)
                     for vals in _input]
