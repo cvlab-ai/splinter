Fix image rotation

inference_engine/src/preprocessing/preprocessing.py
@@ -10,7 +10,8 @@ from src.utils.exceptions import PreprocessingError
 from .extractors import TextExtractor, IndexExtractor, BoxExtractor, FieldExtractor, GroupExtractor
 from .fields import FieldName, Field
 from .rotation import rotate_exam
-
+from .crop import crop_exam
+from random import randint
 
 class Preprocessing:
     FIELD_EXTRACTOR_MAPPING = {
@@ -27,7 +28,12 @@ class Preprocessing:
 
     def process(self) -> tp.Tuple[tp.Dict[FieldName, tp.List[Field]], np.ndarray]:
         self.show_image(self._exam_copy)
-        # self._exam_copy = rotate_exam(self._exam_copy) # Preprocessing do wywalenia (rotate xam
+
+        self._exam_copy = rotate_exam(self._exam_copy)
+        self._exam_copy = crop_exam(self._exam_copy)
+
+        self._exam_copy = cv2.resize(self._exam_copy, (2480, 3508))
+
         # self.show_image(self._exam_copy)
         try:
             fields = FieldExtractor(Field(self._exam_copy)).process() # TODO Przeorbić field Extractor (na podstawie px)