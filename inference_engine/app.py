fb277e93cfcfa3cc8c05aa6123d513dd97eb61fe
Krystian030 <s184589@student.pg.edu.pl>
2024-11-16 14:05:05 +0100

    feat: simplify middleware

--- inference_engine/app.py
+++ inference_engine/app.py
@@ -11,21 +11,6 @@ app.include_router(router)
 
 
 @app.middleware('http')
-async def request_middleware(request: Request, call_next):
-    # Read the request body as bytes
-    body_bytes = await request.body()
-
-    # Decode the bytes to a string for logging
-    body_str = body_bytes.decode('utf-8')
-    logging.info(f'--- New request approach: {request.method} {body_str}')
-
-    # Create a new stream for the request body to make it available downstream
-    async def receive():
-        return {'type': 'http.request', 'body': body_bytes}
-
-    # Replace the request's receive method with the new one
-    request._receive = receive
-
-    # Proceed with the request
-    response = await call_next(request)
-    return response
+def request_middleware(request: Request, call_next):
+    logging.info(f'--- New request approach: {request.method} {request.json}')
+    return call_next(request)
